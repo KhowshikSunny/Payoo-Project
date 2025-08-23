ee641d57748b34a0a71d844517934df6d11f8e85
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-08-23 23:56:59 +0600

    Login functionality complete

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 // login button function 
 document.getElementById('loginButton')
-.addEventListener('click', function(e){
-    e.preventDefault()
-    console.log('login click')
-    
-})
+    .addEventListener('click', function (e) {
+        e.preventDefault()
+        const mobileNumber = 12345678910;
+        const pinNumber = 1234;
+        
+        const mobileNumberValue = document.getElementById('mobile-number').value
+        const mobileNumberValueConvertion = parseInt(mobileNumberValue);
+
+        const pinNumberValue = document.getElementById('pin-number').value;
+        const pinNumberValueConvertion = parseInt(pinNumberValue);
+        
+
+        if (mobileNumberValueConvertion === mobileNumber && pinNumberValueConvertion === pinNumber) {
+            window.location.href = "home.html"
+        } else {
+            alert('Invalid Credential')
+        }
+    })
