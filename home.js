9d3a0e6e3ebe9ab4118a566b9ac4ce96a9ccc937
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-08-24 18:02:02 +0600

    account number and pin validation check

--- home.js
+++ home.js
@@ -1,20 +1,35 @@
+const validPin =1234
+
+
+
+
 document.getElementById('add-money-btn')
     .addEventListener("click", function (e) {
         e.preventDefault();
-        const bank = document.getElementById('bank').value
+        const bank = document.getElementById('bank').value 
+        const accountNumber = document.getElementById('account-number').value 
 
-        const accountNumber = document.getElementById('account-number').value
+        const amount = parseInt(document.getElementById('add-amount').value)
 
-        const addAmount = parseInt(document.getElementById('add-amount').value)
+        const pinNumber = parseInt(document.getElementById('add-pin').value )
 
-        const addPin = parseInt(document.getElementById('add-pin').value)
+         const avalableBalance = parseInt(document.getElementById('available-balance').innerText)
 
-        const avalableBalance = parseInt(document.getElementById('available-balance').innerText)
+        if(accountNumber.length < 11){
+            alert('Please Provide Valid account Number')
+            return;
+        }
+        if(pinNumber !== validPin){
+            alert('Please Provide Valid PIN Number')
+            return;
+        }
+
+       
+
+        const totalNewAvailableBalance = amount + avalableBalance;
+
+        document.getElementById('available-balance').innerText=totalNewAvailableBalance;
 
 
-        const totalNewAvailableBalance = avalableBalance+addAmount;
-        
-        document.getElementById('available-balance')
-        .innerText=totalNewAvailableBalance;
-
+        console.log(avalableBalance , amount, totalNewAvailableBalance)
     })
