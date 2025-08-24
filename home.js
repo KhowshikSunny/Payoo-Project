eb387eed52a902e2f4c295e5a128a6fd53e0e02f
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-08-25 01:36:50 +0600

    reuseable function

--- home.js
+++ home.js
@@ -1,63 +1,77 @@
-const validPin =1234
+const validPin = 1234
 
+// function to get values 
+
+function getInputValueNumber(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue);
+    return inputFieldValueNumber;
+}
+
+function getInputValue(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue;
+
+}
 
 
 // add money feature 
 document.getElementById('add-money-btn')
     .addEventListener("click", function (e) {
         e.preventDefault();
-        const bank = document.getElementById('bank').value 
-        const accountNumber = document.getElementById('account-number').value 
+        const bank = getInputValue('bank')
+        const accountNumber = getInputValue('account-number')
 
-        const amount = parseInt(document.getElementById('add-amount').value)
+        const amount = getInputValueNumber('add-amount')
 
-        const pinNumber = parseInt(document.getElementById('add-pin').value )
+        const pinNumber = getInputValueNumber('add-pin')
 
-         const avalableBalance = parseInt(document.getElementById('available-balance').innerText)
+        const avalableBalance = parseInt(document.getElementById('available-balance').innerText)
 
-        if(accountNumber.length < 11){
+        if (accountNumber.length < 11) {
             alert('Please Provide Valid account Number')
             return;
         }
-        if(pinNumber !== validPin){
+        if (pinNumber !== validPin) {
             alert('Please Provide Valid PIN Number')
             return;
         }
 
-       
+
 
         const totalNewAvailableBalance = amount + avalableBalance;
 
-        document.getElementById('available-balance').innerText=totalNewAvailableBalance;
+        document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 
 
-        console.log(avalableBalance , amount, totalNewAvailableBalance)
+        console.log(avalableBalance, amount, totalNewAvailableBalance)
     })
 
 
-    // cash out feature 
-    document.getElementById('withdraw-btn').addEventListener('click', function(e){
-        e.preventDefault()
-        const amount = parseInt(document.getElementById('withdraw-amount').value )
-        const avalableBalance= parseInt(document.getElementById('available-balance').innerText)
+// cash out feature 
+document.getElementById('withdraw-btn').addEventListener('click', function (e) {
+    e.preventDefault()
+    const amount = getInputValueNumber('withdraw-amount')
+    const avalableBalance = parseInt(document.getElementById('available-balance').innerText)
 
 
-        const totalNewAvailableBalance = avalableBalance - amount;
+    const totalNewAvailableBalance = avalableBalance - amount;
 
-        document.getElementById('available-balance').innerText=totalNewAvailableBalance;
-        console.log(totalNewAvailableBalance)
-
-
-    })
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 
 
-    // toggle feature 
-    document.getElementById('add-button').addEventListener('click', function(){
-        document.getElementById('cashout-parent').style.display = "none"
-        document.getElementById('add-money-parent').style.display = 'block'
-    })
+})
 
-    document.getElementById('cashout-button').addEventListener('click', function(){
-        document.getElementById('add-money-parent').style.display = "none"
-        document.getElementById('cashout-parent').style.display = 'block'
-    })
+
+// toggle feature 
+document.getElementById('add-button').addEventListener('click', function () {
+    document.getElementById('cashout-parent').style.display = "none"
+    document.getElementById('add-money-parent').style.display = 'block'
+})
+
+document.getElementById('cashout-button').addEventListener('click', function () {
+    document.getElementById('add-money-parent').style.display = "none"
+    document.getElementById('cashout-parent').style.display = 'block'
+})
